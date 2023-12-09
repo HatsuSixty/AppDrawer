9e65dcd5f81a8c99091c46c95ef1a7970dc94744
HatsuSixty <hatsunotsus@gmail.com>
2023-12-09 18:39:25 -0300

    Document RudeDrawer commands

--- Include/RudeDrawer.h
+++ Include/RudeDrawer.h
@@ -7,13 +7,43 @@
 // RGBA
 #define COMPONENTS 4
 
+// These are all of the command types that can be sent to AppDrawer.
+// The client should NOT wait for a response from commands that don't return anything.
 typedef enum {
+    // Makes the server print "Pong!" in its logs.
+    // Required arguments: None
+    // Returns: `RDRESP_EMPTY`
     RDCMD_PING,
+    // Adds a Window.
+    // Required arguments:
+    //   - `windowDims`
+    //   - `windowTitle` (has to fit in `WINDOW_TITLE_MAX`)
+    // Returns: `RDRESP_WINID`
     RDCMD_ADD_WIN,
+    // Removes a window.
+    // Required arguments:
+    //   - `windowId`
+    // Returns: `RDRESP_EMPTY`
     RDCMD_REMOVE_WIN,
+    // Makes the server start sending events to the client.
+    // Required arguments:
+    //   - `windowId`
+    // Returns: `RDRESP_EMPTY`
     RDCMD_START_POLLING_EVENTS_WIN,
+    // Makes the server stop sending events to the client.
+    // Required arguments:
+    //   - `windowId`
+    // Returns: `RDRESP_EMPTY`
     RDCMD_STOP_POLLING_EVENTS_WIN,
+    // Returns the name of a shared memory that contains the pixels of the specified window (specified by `windowId`).
+    // Required arguments:
+    //   - `windowId`
+    // Returns: `RDRESP_SHM_NAME`
     RDCMD_GET_DISPLAY_SHM_WIN,
+    // Makes the server send a paint event to the specified window (specified by `windowId`).
+    // Required arguments:
+    //   - `windowId`
+    // Returns: None
     RDCMD_SEND_PAINT_EVENT,
 } RudeDrawerCommandKind;
 
