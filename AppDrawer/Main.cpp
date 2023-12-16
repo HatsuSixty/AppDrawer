7342503ab059f5b002d81f4e18a69925b5b02504
HatsuSixty <hatsunotsus@gmail.com>
2023-12-16 15:31:03 -0300

    Move title bars into their own function

--- AppDrawer/Main.cpp
+++ AppDrawer/Main.cpp
@@ -10,6 +10,80 @@
 #define BORDER_THICKNESS 5
 #define TITLEBAR_THICKNESS 20.0f
 
+void closeButton(AppDrawer* appdrawer, Window* window, Rectangle titleBarRect) noexcept(true)
+{
+    auto active = window->id == appdrawer->windows.back()->id;
+
+    Rectangle closeButtonRect = {
+        .x = titleBarRect.x,
+        .y = titleBarRect.y,
+        .width = TITLEBAR_THICKNESS,
+        .height = TITLEBAR_THICKNESS,
+    };
+    DrawRectangleRec(closeButtonRect, RED);
+
+    // Close button logic
+    if (CheckCollisionPointRec(GetMousePosition(), closeButtonRect)
+        && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)
+        && active) {
+        RudeDrawerEvent event;
+        event.kind = RDEVENT_CLOSE_WIN;
+        window->sendEvent(event);
+    }
+}
+
+void titleBar(AppDrawer* appdrawer, Window* window) noexcept(true)
+{
+    auto active = window->id == appdrawer->windows.back()->id;
+
+    Rectangle titleBarRect = {
+        .x = window->area.x - BORDER_THICKNESS,
+        .y = window->area.y - BORDER_THICKNESS - TITLEBAR_THICKNESS,
+        .width = window->area.width + BORDER_THICKNESS * 2,
+        .height = TITLEBAR_THICKNESS,
+    };
+    DrawRectangleRec(titleBarRect, YELLOW);
+
+    closeButton(appdrawer, window, titleBarRect);
+
+    DrawText(window->title.c_str(), window->area.x + TITLEBAR_THICKNESS + 1, titleBarRect.y,
+        titleBarRect.height, BLUE);
+
+    if (CheckCollisionPointRec(GetMousePosition(), titleBarRect)) {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            window->isDragging = true;
+        }
+    }
+
+    if (window->isDragging) {
+        if (active) {
+            auto delta = GetMouseDelta();
+            window->area.x += delta.x;
+            window->area.y += delta.y;
+        }
+
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+            window->isDragging = false;
+        }
+    }
+}
+
+void windowBorders(Window* window) noexcept(true)
+{
+    auto border = window->area;
+    border.width += BORDER_THICKNESS * 2;
+    border.height += BORDER_THICKNESS * 2;
+    border.x -= BORDER_THICKNESS;
+    border.y -= BORDER_THICKNESS;
+    DrawRectangleLinesEx(border, BORDER_THICKNESS, BLUE);
+}
+
+void windowDecoration(AppDrawer* appdrawer, Window* window) noexcept(true)
+{
+    windowBorders(window);
+    titleBar(appdrawer, window);
+}
+
 int main() noexcept(true)
 {
     signal(SIGPIPE, SIG_IGN);
@@ -28,8 +102,6 @@ int main() noexcept(true)
         appdrawer->windowsMutex.lock();
         for (auto& w : appdrawer->windows) {
             BeginScissorMode(w->area.x, w->area.y, w->area.width, w->area.height);
-
-            // Draw content
             Image image = {
                 .data = w->pixels,
                 .width = (int)w->area.width,
@@ -39,68 +111,9 @@ int main() noexcept(true)
             };
             texture = LoadTextureFromImage(image);
             DrawTexture(texture, w->area.x, w->area.y, WHITE);
-
-            // Draw title
-            DrawText(w->title.c_str(), w->area.x + 1, w->area.y, 30, BLUE);
-
             EndScissorMode();
 
-            // Draw borders
-            auto border = w->area;
-            border.width += BORDER_THICKNESS * 2;
-            border.height += BORDER_THICKNESS * 2;
-            border.x -= BORDER_THICKNESS;
-            border.y -= BORDER_THICKNESS;
-            DrawRectangleLinesEx(border, BORDER_THICKNESS, BLUE);
-
-            auto active = w->id == appdrawer->windows.back()->id;
-
-            // Draw title bar
-            Rectangle titleBarRect = {
-                .x = w->area.x - BORDER_THICKNESS,
-                .y = w->area.y - BORDER_THICKNESS - TITLEBAR_THICKNESS,
-                .width = w->area.width + BORDER_THICKNESS * 2,
-                .height = TITLEBAR_THICKNESS,
-            };
-            DrawRectangleRec(titleBarRect, YELLOW);
-
-            // Title bar logic
-            if (CheckCollisionPointRec(GetMousePosition(), titleBarRect)) {
-                if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-                    w->isDragging = true;
-                }
-            }
-
-            if (w->isDragging) {
-                if (active) {
-                    auto delta = GetMouseDelta();
-                    w->area.x += delta.x;
-                    w->area.y += delta.y;
-                }
-
-                if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-                    w->isDragging = false;
-                }
-            }
-
-            // Draw close button
-            auto closeButtonDims = 20.0f;
-            Rectangle closeButtonRect = {
-                .x = w->area.x - BORDER_THICKNESS,
-                .y = w->area.y - BORDER_THICKNESS - closeButtonDims,
-                .width = closeButtonDims,
-                .height = closeButtonDims,
-            };
-            DrawRectangleRec(closeButtonRect, RED);
-
-            // Close button logic
-            if (CheckCollisionPointRec(GetMousePosition(), closeButtonRect)
-                && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)
-                && active) {
-                RudeDrawerEvent event;
-                event.kind = RDEVENT_CLOSE_WIN;
-                w->sendEvent(event);
-            }
+            windowDecoration(appdrawer, w);
         }
         appdrawer->windowsMutex.unlock();
 
@@ -109,8 +122,8 @@ int main() noexcept(true)
                 auto windowArea = w->area;
                 windowArea.y -= BORDER_THICKNESS + TITLEBAR_THICKNESS;
                 windowArea.x -= BORDER_THICKNESS;
-                windowArea.width += BORDER_THICKNESS*2;
-                windowArea.height += TITLEBAR_THICKNESS + BORDER_THICKNESS*2;
+                windowArea.width += BORDER_THICKNESS * 2;
+                windowArea.height += TITLEBAR_THICKNESS + BORDER_THICKNESS * 2;
                 if (CheckCollisionPointRec(GetMousePosition(), windowArea)) {
                     if (w->id != appdrawer->windows.back()->id) {
                         appdrawer->changeActiveWindow(w->id);
